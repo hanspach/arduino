ba0fbbfb5b36c7f4fe5e33a71857c8453bd5df4b
hanspach <hsoft@gmx.net>
2023-06-15 13:30:27 +0200

    uart/trans ds1820 added

--- uart/transmitter/src/main.cpp
+++ uart/transmitter/src/main.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <HardwareSerial.h>
 #define TXD_PIN 17
 #define RXD_PIN 16
 
+const uint8_t BUS = 4;
+OneWire oneWire(BUS);
+DallasTemperature sensor(&oneWire);
+
 void setup() {
+#ifdef _DEBUG_
   Serial.begin(9600);
   if(!Serial) {
     while(1);
   } 
-
+#endif
   Serial2.begin(9600,SERIAL_8N1,RXD_PIN, TXD_PIN);
   if(!Serial2) {
+#ifdef _DEBUG_
     Serial.println("Can't init Serial2");
+#endif
+    while(1);
+  }
+  sensor.begin();
+  if(!sensor.isConnected(&BUS)) {
+#ifdef _DEBUG_
+    Serial.println("Can't init DS1820");
+#endif
+    while(1);
   }
 }
 
