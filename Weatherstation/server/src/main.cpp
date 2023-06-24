8b93a68c24ac9ccdefcc8fc56a7c75975954c1ad
hanspach <hsoft@gmx.net>
2023-06-24 08:25:01 +0200

    dcf added

--- Weatherstation/server/src/main.cpp
+++ Weatherstation/server/src/main.cpp
@@ -1,76 +1,59 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_MCP9808.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include <esp_bt_main.h>
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <HardwareSerial.h>
+#include <dcf77.h>
+#define TXD_PIN 17
+#define RXD_PIN 16
+#define DCF_PIN 12
+#define _DEBUG_
 
-Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
-char txtVal[10] = "\0";
-bool deviceConnected = false;
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic;
-//RTC_DATA_ATTR unsigned int count = 1;
-
-class MyServerCallbacks: public BLEServerCallbacks {
-	void onConnect(BLEServer* pServer) {
-	  deviceConnected = true;
-	}
-	
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
-};
+const uint8_t BUS = 4;
+const char* DAYS[] = {"","Mo","Di","Mi","Do","Fr","Sa","So"};
+OneWire oneWire(BUS);
+DallasTemperature sensor(&oneWire);
 
 void setup() {
-  //pinMode(LED_BUILTIN, OUTPUT);
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //delay(500);
-  //Serial.begin(9600);
-  esp_sleep_enable_timer_wakeup(1000000ULL * 60);
-  //Serial.print("Setup started: ");
-  tempsensor.begin(0x18);                           // default address
-  tempsensor.setResolution(1);                      //
+#ifdef _DEBUG_
+  Serial.begin(9600);
+  if(!Serial) {
+    while(1);
+  } 
+#endif
   
-  BLEDevice::init("SENS");               // Create the BLE Device
-  while(!BLEDevice::getInitialized()) {
-     delay(10);
+  DCF77::Start(DCF_PIN);
+  Serial2.begin(9600,SERIAL_8N1,RXD_PIN, TXD_PIN);
+  if(!Serial2) {
+#ifdef _DEBUG_
+    Serial.println("Can't init Serial2");
+#endif
+    while(1);
   }
-  pServer = BLEDevice::createServer();              // Create the BLE Server
-  BLEService* pService = pServer->createService(SERVICE_UUID);  // Create the BLE Service
-  pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID,
-  BLECharacteristic::PROPERTY_READ|BLECharacteristic::PROPERTY_WRITE|
-	BLECharacteristic::PROPERTY_NOTIFY|BLECharacteristic::PROPERTY_INDICATE);
-  pCharacteristic->addDescriptor(new BLE2902());
-  pService->start();                               // Start the service
-  BLEAdvertising* pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
-  pAdvertising->setMinPreferred(0x12);
-  BLEDevice::startAdvertising();
-  if(deviceConnected) {
-	  tempsensor.wake();                                // wake up, ready to read!
-	  float f = tempsensor.readTempC();
-	  int c = (int)f;                                  // round value
-	  sprintf(txtVal,"%2d", c);
-	  pCharacteristic->setValue(txtVal);
-	  pCharacteristic->notify();
-	  delay(500);                                    // enough time to send data
-	  tempsensor.shutdown_wake(1);
-	  //Serial.printf("BLE sends %s°C for %d times.\n",txtVal,count);
-	  //++count;
-  }
-  BLEDevice::deinit();
-  while(BLEDevice::getInitialized()) {              // disable BLE
-    delay(10);
-  }
-  esp_deep_sleep_start();
+
+  sensor.begin();
 }
 
 void loop() {
+  char buffer[36] = "\0";
+
+  if(Serial2.availableForWrite()) {
+    sensor.requestTemperatures();
+    float celsius = sensor.getTempCByIndex(0);
+    int c = (int)celsius;
+    time_t t = DCF77::getTime();
+    if(t) {
+      tm dt = toTmStruct(t);
+      if(dt.tm_wday > 0 && dt.tm_wday <= 7) 
+      sprintf(buffer,"%s, %d.%d.%d %d:%d-%d",DAYS[dt.tm_wday],dt.tm_mday,dt.tm_mon,dt.tm_year,dt.tm_hour,dt.tm_min,c);
+    else
+      sprintf(buffer,"%d.%d.%d %d:%d-%d",dt.tm_mday,dt.tm_mon,dt.tm_year,dt.tm_hour,dt.tm_min,c);
+    } else {
+      sprintf(buffer,"2%d",c);
+    }
+    Serial2.write(buffer,strlen(buffer));
+#ifdef _DEBUG_
+    Serial.printf("%s ",buffer);
+#endif
+  }
+  delay(1000);
 }
